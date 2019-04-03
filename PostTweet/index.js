4c0318874d56a4939c7d4cc170b81e63cd07ab59
Tatsuya Sato <tatsuya.b.sato@rakuten.com>
2019-04-03 12:29:15 +0900

    Implemented PostTweet API

---
+++ PostTweet/index.js
@@ -0,0 +1,21 @@
+const Twitter = require('twitter')
+
+module.exports = async function (context, req) {
+    const status = req.body.status
+    const twitter = new Twitter({
+        consumer_key: process.env.TWITTER_CONSUMER_KEY,
+        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+        access_token_key: req.headers['x-ms-token-twitter-access-token'],
+        access_token_secret: req.headers['x-ms-token-twitter-access-token-secret']
+    })
+
+    try {
+        const tweet = await twitter.post('statuses/update', { status })
+        context.bindings.res = { body: tweet }
+
+        context.done()
+    } catch(err) {
+        context.log.error(err)
+        context.done(err)
+    }
+};
