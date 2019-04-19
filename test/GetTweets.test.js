4b6c9bd116559c1ba3d55dc6a7d2ef2c22881ee0
Tatsuya Sato <satoryu.1981@gmail.com>
2019-04-19 15:18:58 +0900

    Use describe and it, looks like RSpec :)

--- test/GetTweets.test.js
+++ test/GetTweets.test.js
@@ -1,16 +1,18 @@
 const func = require('../GetTweets')
 
-test('should return 401 if the user does not pass easyAuth', async () => {
-    const context = {
-        bindings: {
-            res: {}
-        },
-        done() {}
-    }
-    const req = {
-        headers: { 'x-ms-client-principal-name': null }
-    }
+describe('GetTweets', () => {
+    it('should return 401 if the user does not pass easyAuth', async () => {
+        const context = {
+            bindings: {
+                res: {}
+            },
+            done() {}
+        }
+        const req = {
+            headers: { 'x-ms-client-principal-name': null }
+        }
 
-    await func(context, req)
-    expect(context.bindings.res.status).toBe(401)
+        await func(context, req)
+        expect(context.bindings.res.status).toBe(401)
+    })
 })
