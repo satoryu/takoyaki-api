631fe0af6649cd603b6a3927ff665fb5b1e230a0
Tatsuya Sato <satoryu.1981@gmail.com>
2019-04-19 14:24:58 +0900

    Add a script to measure code coverage in test

--- jest.config.js
+++ jest.config.js
@@ -21,7 +21,11 @@ module.exports = {
   // collectCoverage: false,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: null,
+  collectCoverageFrom: [
+    "**/*.js",
+    "!*.config.js",
+    "!**/{node_modules,test,coverage}/**"
+  ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
@@ -109,7 +113,7 @@ module.exports = {
   // restoreMocks: false,
 
   // The root directory that Jest should scan for tests and modules within
-  rootDir: 'test/',
+  // rootDir: null
 
   // A list of paths to directories that Jest should use to search for files in
   // roots: [
