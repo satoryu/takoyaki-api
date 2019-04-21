01a4d9c562ed74fa079ad8ff80f8cce67eef8af1
Tatsuya Sato <satoryu.1981@gmail.com>
2019-04-22 01:26:29 +0900

    Refactoring: remove contexst var

--- GetTweets/index.js
+++ GetTweets/index.js
@@ -1,20 +1,20 @@
-module.exports = async function (context, req) {
+module.exports = async function ({ bindings, done }, req) {
     const auth_user_name = req.headers['x-ms-client-principal-name']
 
     if (!auth_user_name) {
-        context.bindings.res = {
+        bindings.res = {
             status: 401
         }
-        context.done()
+        done()
         return
     }
 
-    const loadedData = context.bindings.tweets
+    const loadedData = bindings.tweets
     if (loadedData.length == 0) {
-        context.bindings.res = {
+        bindings.res = {
             status: 404
         }
-        context.done()
+        done()
         return
     }
 
@@ -27,9 +27,9 @@ module.exports = async function (context, req) {
         }
     })
 
-    context.bindings.res = {
+    bindings.res = {
         status: 200,
         body: tweets
     }
-    context.done();
+    done();
 }
